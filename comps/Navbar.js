3d0e5ecd80b1cbbac368587bab17259a4e5e2145
nourHellani <nourelhellani@gmail.com>
2023-01-24 14:39:31 +0200

    test

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
-
+import Image from "next/image";
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>Ninja List</h1>
+        <Image src="/logo.png" alt="site logo" width={128} height={77} />
       </div>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
