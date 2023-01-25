d8a3241b72785b9a806bbddf86e2f47bfcbc5826
nourHellani <nourelhellani@gmail.com>
2023-01-25 14:54:55 +0200

    test

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -16,10 +16,10 @@ const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
-      <div className="  grid gap-4 grid-cols-4">
+      <div className="  grid gap-2 grid-cols-4">
         {ninjas.map((ninja) => (
           <Link
-            className=" px-2 py-4  mx-5 my-3 border border-l-3 border-l-white  hover:border-l-[#4979ff] bg-[#fff]"
+            className=" px-3 py-4  mx-2 my-3 border border-l-3 border-l-white  hover:border-l-[#4979ff] bg-[#fff]"
             href={"/ninjas/" + ninja.id}
             key={ninja.id}
           >
