3f200a55165b35eff17f49646fa99aa7d75924c7
nourHellani <nourelhellani@gmail.com>
2023-01-26 12:15:33 +0200

    test

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,6 +1,23 @@
-import React, { useState } from "react";
 import Datepicker from "react-tailwindcss-datepicker";
+import { useState } from "react";
+import * as React from "react";
+import dayjs from "dayjs";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+
 const Calender = () => {
+  const [date, setDate] = React.useState(dayjs("2014-08-18T21:11:54"));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
   const [value, setValue] = useState({
     startDate: null,
     endDate: null,
@@ -11,11 +28,44 @@ const Calender = () => {
   };
   return (
     <div>
-      <Datepicker
-        value={value}
-        onChange={handleValueChange}
-        showShortcuts={true}
-      />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Stack spacing={3}>
+          <DesktopDatePicker
+            label="Date desktop"
+            inputFormat="MM/DD/YYYY"
+            value={date}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <MobileDatePicker
+            label="Date mobile"
+            inputFormat="MM/DD/YYYY"
+            value={date}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <TimePicker
+            label="Time"
+            value={date}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <DateTimePicker
+            label="Date&Time picker"
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Stack>
+      </LocalizationProvider>
+
+      <div className="py-5">
+        <Datepicker
+          value={value}
+          onChange={handleValueChange}
+          showShortcuts={true}
+        />
+      </div>
     </div>
   );
 };
