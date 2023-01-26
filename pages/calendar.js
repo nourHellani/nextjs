b48ea018c20e735bb2d319f82d5e8f23364ef333
nourHellani <nourelhellani@gmail.com>
2023-01-26 11:33:24 +0200

    test

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,32 +1,16 @@
-import Head from "next/head";
 import React, { useState } from "react";
 import Datepicker from "react-tailwindcss-datepicker";
-import { TimePicker } from "react-ios-time-picker";
-import Timeit from "react-timeit";
-
 const Calender = () => {
-  const [time, setTime] = useState("10:00 AM");
-
-  const onChange = (timeValue) => {
-    setTime(timeValue);
-  };
-
   const [value, setValue] = useState({
     startDate: null,
     endDate: null,
   });
-
   const handleValueChange = (newValue) => {
     console.log("newValue:", newValue);
     setValue(newValue);
   };
-
   return (
     <div>
-      <div className="flex justify-center py-9">
-        <TimePicker onChange={onChange} time={value} use12Hours />
-      </div>
-
       <Datepicker
         value={value}
         onChange={handleValueChange}
